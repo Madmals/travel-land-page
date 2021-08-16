151b1a2e4001d9b2032efe606e3ab3faec93fea3
k@m@l <kabota000@gmail.com>
2021-08-16 15:27:00 +0100

    marker api

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ let imgs = document.querySelector('.image_all')
 
 const test_api = async () => {
 
-	let place = document.querySelector(".location").value
 
+	let place = document.querySelector(".location").value
 
 	let res = await fetch(`https://api.unsplash.com/search/photos?client_id=5DUHnuUyhRR32OnW2yeGm4XRa0jfJBmabeD1Relm8c4&query=${place}?page=1`)
 	let data = await res.json()
 	let a = data.results.map(urls => `${urls.urls.raw}?&w=768&h=500&fit=crop`)
-	
+
 
 
 
@@ -52,11 +52,11 @@ button.addEventListener('click', () => {
 
 //api compliment
 let rev = document.querySelector(".review")
-const compliment = async()=>{
+const compliment = async () => {
 	let res = await fetch("https://complimentr.com/api")
 	let data = await res.json()
 	rev.innerHTML = data.compliment
-	
+
 }
 
 
@@ -64,25 +64,144 @@ const compliment = async()=>{
 //api avatar
 
 let avatars = document.querySelector(".avatarimg")
-const avatar = async()=>{
+const avatar = async () => {
 	let res = await fetch("https://randomuser.me/api/?page=3&results=10")
 	let data = await res.json()
-	data.results.map(eahcone=>
-		{let pic = eahcone.picture.large
+	data.results.map(eahcone => {
+		let pic = eahcone.picture.large
 		avatars.src = pic
-		})
-	
+	})
+
 }
 
+//api place information
+
+function initMap(){
+	let places = new google.maps.LatLng(3.1390, 101.6869);
+	let map = new google.maps.Map(document.getElementById('map'), {
+		center: places,
+		zoom: 15,
+		mapId: "8d193001f940fde3"
+	});
+
+	let request = {
+		location: places,
+		radius: '500',
+		type: ['restaurant']
+	};
+
+	service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(request, callback);
+}
+
+const callback = (results, status) => {
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for (let i = 0; i < results.length; i++) {
+			createMarker(results[i]);
+		}
+	}
+}
+
+const createMarker = (data) => {
+	const placesList = document.getElementById('places')
+	let image = {
+		url: data.icon,
+		size: new google.maps.Size(200, 71),
+		origin: new google.maps.Point(0, 0),
+		anchor: new google.maps.Point(17, 34),
+		scaledSize: new google.maps.Size(100, 25),
+	};
+
+
+	new google.maps.Marker({
+		map,
+		icon: image,
+		title: data.name,
+		position: data.geometry.location,
+	})
+	const li = document.createElement("li");
+	li.textContent = data.name;
+	placesList.appendChild(li);
+
+
+}
+
+// function initMap() {
+// 	// Create the map.
+// 	const pyrmont = { lat: -33.866, lng: 151.196 };
+// 	const map = new google.maps.Map(document.getElementById("map"), {
+// 		center: pyrmont,
+// 		zoom: 17,
+// 		mapId: "8d193001f940fde3",
+// 	});
+// 	// Create the places service.
+// 	const service = new google.maps.places.PlacesService(map);
+// 	let getNextPage;
+// 	const moreButton = document.getElementById("more");
+
+// 	moreButton.onclick = function () {
+// 		moreButton.disabled = true;
+
+// 		if (getNextPage) {
+// 			getNextPage();
+// 		}
+// 	};
+// 	// Perform a nearby search.
+// 	service.nearbySearch(
+// 		{ location: pyrmont, radius: 500, type: "store" },
+// 		(results, status, pagination) => {
+// 			if (status !== "OK" || !results) return;
+// 			addPlaces(results, map);
+// 			moreButton.disabled = !pagination || !pagination.hasNextPage;
+
+// 			if (pagination && pagination.hasNextPage) {
+// 				getNextPage = () => {
+// 					// Note: nextPage will call the same handler function as the initial call
+// 					pagination.nextPage();
+// 				};
+// 			}
+// 		}
+// 	);
+// }
+
+// function addPlaces(places, map) {
+// 	const placesList = document.getElementById("places");
+
+// 	for (const place of places) {
+// 		if (place.geometry && place.geometry.location) {
+// 			const image = {
+// 				url: place.icon,
+// 				size: new google.maps.Size(71, 71),
+// 				origin: new google.maps.Point(0, 0),
+// 				anchor: new google.maps.Point(17, 34),
+// 				scaledSize: new google.maps.Size(25, 25),
+// 			};
+// 			new google.maps.Marker({
+// 				map,
+// 				icon: image,
+// 				title: place.name,
+// 				position: place.geometry.location,
+// 			});
+// 			const li = document.createElement("li");
+// 			li.textContent = place.name;
+// 			placesList.appendChild(li);
+// 			li.addEventListener("click", () => {
+// 				map.setCenter(place.geometry.location);
+// 			});
+// 		}
+// 	}
+// }
+
+
+
+
+setInterval(() => {
+	compliment()
+	avatar()
+}, 8000)
 
 
 test_api()
-
-setInterval(()=>{compliment()
-avatar()
-},8000)
-
-
 
 
 //nav on scroll change background
